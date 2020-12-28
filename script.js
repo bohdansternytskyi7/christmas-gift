6ff6b4aa78ef7c9ada217bd45e2f349787530867
bohdan <bohdansternytskyi@gmail.com>
2020-12-28 01:28:47 +0100

    finishing box switching function

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 // Selectors
 const openBtnEl = document.querySelector('.btn-open');
+const nextBtnEl = document.querySelector('.btn-next');
+const giftEl = document.querySelector('.gift');
 const capEl = document.querySelector('.cap');
 
 const openGift = () => {
   capEl.classList.add('open');
+  openBtnEl.disabled = true;
+};
+
+const nextGift = () => {
+  nextBtnEl.disabled = true;
+  giftEl.classList.add('fall');
+  capEl.classList.remove('open');
+  setTimeout(() => {
+    giftEl.classList.remove('fall');
+    giftEl.classList.add('appear');
+    setTimeout(() => {
+      giftEl.classList.remove('appear');
+      nextBtnEl.disabled = false;
+      openBtnEl.disabled = false;
+    }, 1000);
+  }, 1000);
 };
 
 openBtnEl.addEventListener('click', openGift);
+nextBtnEl.addEventListener('click', nextGift);
